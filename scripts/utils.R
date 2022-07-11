8eedc13af695170d6b04298a50c276336039fb26
JennaCollier <jcollier@g.harvard.edu>
2022-07-11 17:26:51 -0400

    changed cluster annotations

--- scripts/utils.R
+++ scripts/utils.R
@@ -1,6 +1,6 @@
 ## utilities for the NOD scRNA-seq analysis
 
-#custom color palettes for various plots
+## custom color palettes for various plots
 custom_colors <- function(pal) {
   
   if (pal == "tissues") {
@@ -28,30 +28,40 @@ custom_colors <- function(pal) {
   }
   
   if (pal == "clusters") {
-    return(c("CD8_cm_Gzmm" = "#612A79FF",
-             "CD8_mem" = "#5DB1DDFF",
-             "CD8_slec" = "#BA6338FF",
-             "CD8_cm" = "#466983FF",
-             "CD8_eff" = "#F0E685FF",
-             "CD8_pexh" = "#749B58FF",
-             "CD8_texh" = "#CE3D32FF",
-             "Tcon_mem" = "#5A655EFF",
-             "Tcon_eff_Cd200" = "#E7C76FFF",
-             "Tcon_cm" = "#AE1F63FF",
-             "Tcon_effmem" = "#3B1B53FF",
-             "Tcon_fh" = "#D58F5CFF",
-             "Tcon_eff_Ifngr1" = "#C75127FF",
-             "Tcon_eff_Lgals1" = "#924822FF",
-             "Tcon_eff_Cxcr6" = "#6BD76BFF",
-             "Treg_Tigit_lo" = "#CDDEB7FF",
+    return(c("CD8_cm" = "#837B8DFF",
+             "CD8_cm_Gzmm" = "#CE3D32FF",
+             "CD8_mem" = "#749B58FF",
+             "CD8_pexh" = "#F0E685FF",
+             "CD8_effmem" = "#466983FF",
+             "CD8_eff_Cxcr6" = "#BA6338FF",
+             "CD8_slec" = "#5DB1DDFF",
+             "CD8_texh" = "#802268FF",
+             "Tcon_cm" = "#6BD76BFF",
+             "Tcon_mem" = "#D595A7FF",
+             "Tcon_effmem" = "#924822FF",
+             "Tcon_fh" = "#5050FFFF",
+             "Tcon_Nrn1" = "#C75127FF",
+             "Tcon_eff_Cxcr6" = "#D58F5CFF",
+             "Treg_Tigit_lo" = "#7A65A5FF",
              "Treg_Tigit_hi" = "#E4AF69FF",
-             "Recently_activated" = "#7A65A5FF",
-             "Integrin_associated" = "#837B8DFF",
-             "Acinar_contaminated" = "#D595A7FF",
-             "Interferon_sensing" = "#802268FF",
-             "Proliferating" = "#5050FFFF"))
+             "Recently_activated" = "#3B1B53FF",
+             "Acinar_contaminated" = "#CDDEB7FF",
+             "Interferon_sensing" = "#612A79FF",
+             "Proliferating" = "#AE1F63FF"))
   } else {
     
   stop("Invalid palette. Options: clusters, pm, treatments, tissues")
   }
+}
+
+## conversion of human gene symbols to mouse gene symbols
+convertHumanGeneList <- function(x){
+  require("biomaRt")
+  human = useMart(biomart="ensembl", dataset = "hsapiens_gene_ensembl", verbose = TRUE, host = "dec2021.archive.ensembl.org")
+  mouse = useMart(biomart="ensembl", dataset = "mmusculus_gene_ensembl", verbose = TRUE, host = "dec2021.archive.ensembl.org")
+  genesV2 = getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = cell_cycle, mart = human, attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=T)
+  humanx <- unique(genesV2[, 2])
+  # Print the first 6 genes found to the screen
+  print(head(humanx))
+  return(humanx)
 }
