8497bbd654c23819fec9560c599990e0c4782964
JennaCollier <jcollier@g.harvard.edu>
2022-07-11 12:15:46 -0400

    added another color palette

--- scripts/utils.R
+++ scripts/utils.R
@@ -20,30 +20,38 @@ custom_colors <- function(pal) {
               "pancreas & periphery" = "#1f78b4",
               "periphery only" = "#BB5566"))
   }
+  
+  if (pal == "celltypes") {
+    return(c("CD8" = "#fe4a49",
+             "Tcon" = "#011f4b",
+             "Treg" = "#6497b1"))
+  }
+  
   if (pal == "clusters") {
-    return(c("Tcon_mem" = "#5A655EFF",
-             "Tcon_eff_Cd200" = "#E7C76FFF",
-             "Tcon_cm" = "#AE1F63FF",
-             "CD8_cm_Gzmm" = "#612A79FF",
-             "Treg_Tigit_lo" = "#CDDEB7FF",
-             "Tcon_effmem" = "#3B1B53FF",
-             "Treg_Tigit_hi" = "#E4AF69FF",
-             "Recently_activated" = "#7A65A5FF",
-             "Tcon_fh" = "#D58F5CFF",
-             "Tcon_eff_Ifngr1" = "#C75127FF",
-             "Integrin_associated" = "#837B8DFF",
-             "Tcon_eff_Lgals1" = "#924822FF",
-             "Acinar_contaminated" = "#D595A7FF",
-             "Tcon_eff_Cxcr6" = "#6BD76BFF",
-             "Interferon_sensing" = "#802268FF",
+    return(c("CD8_cm_Gzmm" = "#612A79FF",
              "CD8_mem" = "#5DB1DDFF",
              "CD8_slec" = "#BA6338FF",
              "CD8_cm" = "#466983FF",
              "CD8_eff" = "#F0E685FF",
              "CD8_pexh" = "#749B58FF",
              "CD8_texh" = "#CE3D32FF",
+             "Tcon_mem" = "#5A655EFF",
+             "Tcon_eff_Cd200" = "#E7C76FFF",
+             "Tcon_cm" = "#AE1F63FF",
+             "Tcon_effmem" = "#3B1B53FF",
+             "Tcon_fh" = "#D58F5CFF",
+             "Tcon_eff_Ifngr1" = "#C75127FF",
+             "Tcon_eff_Lgals1" = "#924822FF",
+             "Tcon_eff_Cxcr6" = "#6BD76BFF",
+             "Treg_Tigit_lo" = "#CDDEB7FF",
+             "Treg_Tigit_hi" = "#E4AF69FF",
+             "Recently_activated" = "#7A65A5FF",
+             "Integrin_associated" = "#837B8DFF",
+             "Acinar_contaminated" = "#D595A7FF",
+             "Interferon_sensing" = "#802268FF",
              "Proliferating" = "#5050FFFF"))
   } else {
+    
   stop("Invalid palette. Options: clusters, pm, treatments, tissues")
   }
 }
