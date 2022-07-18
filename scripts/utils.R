c974ecfa93aed1207d9e878a4d9c0b2b68a9a029
JennaCollier <jcollier@g.harvard.edu>
2022-07-18 19:25:56 -0400

    renamed and merged clusters

--- scripts/utils.R
+++ scripts/utils.R
@@ -20,6 +20,13 @@ custom_colors <- function(pal) {
               "non-PM" = "#33a02c"))
   }
   
+  if (pal == "colitis") {
+    return(c("healthy" = "black",
+             "ICI" = "#6B1937",
+             "ICI+colitis" = "#D95D8B",
+             "colitis" = "#502F2C"))
+  }
+  
   if (pal == "tetramer") {
     return(c("unknown" = "grey90",
              "NRPv7" = "Firebrick"))
@@ -32,22 +39,21 @@ custom_colors <- function(pal) {
   }
   
   if (pal == "clusters") {
-    return(c("CD8_cm" = "#837B8DFF",
-             "CD8_cm_Gzmm" = "#CE3D32FF",
+    return(c("CD8_cm" = "#00CC99FF",
              "CD8_mem" = "#749B58FF",
-             "CD8_pexh" = "#F0E685FF",
+             "CD8_pexh" = "#CE3D32FF",
              "CD8_effmem" = "#466983FF",
-             "CD8_eff_Cxcr6" = "#BA6338FF",
+             "CD8_eff" = "#BA6338FF",
              "CD8_slec" = "#5DB1DDFF",
-             "CD8_texh" = "#802268FF",
+             "CD8_texh" = "#1A0099FF",
              "Tcon_cm" = "#6BD76BFF",
              "Tcon_mem" = "#D595A7FF",
              "Tcon_effmem" = "#924822FF",
-             "Tcon_fh" = "#5050FFFF",
-             "Tcon_Nrn1" = "#C75127FF",
-             "Tcon_eff_Cxcr6" = "#D58F5CFF",
-             "Treg_Tigit_lo" = "#7A65A5FF",
-             "Treg_Tigit_hi" = "#E4AF69FF",
+             "Tcon_Tfh" = "#5050FFFF",
+             "Tcon_Th21" = "#C75127FF",
+             "Tcon_eff" = "#D58F5CFF",
+             "Treg_resting" = "#E4AF69FF",
+             "Treg_eff" = "#D60047FF",
              "Recently_activated" = "#3B1B53FF",
              "Acinar_contaminated" = "#CDDEB7FF",
              "Interferon_sensing" = "#612A79FF",
@@ -61,13 +67,25 @@ custom_colors <- function(pal) {
 ## conversion of human gene symbols to mouse gene symbols
 convertHumanGeneList <- function(x){
   require("biomaRt")
-  human = useMart(biomart="ensembl", dataset = "hsapiens_gene_ensembl", verbose = TRUE, host = "dec2021.archive.ensembl.org")
-  mouse = useMart(biomart="ensembl", dataset = "mmusculus_gene_ensembl", verbose = TRUE, host = "dec2021.archive.ensembl.org")
-  genesV2 = getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = cell_cycle, mart = human, attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=T)
+  human = useMart(biomart="ensembl", dataset = "hsapiens_gene_ensembl", verbose = TRUE, host = "https://dec2021.archive.ensembl.org")
+  mouse = useMart(biomart="ensembl", dataset = "mmusculus_gene_ensembl", verbose = TRUE, host = "https://dec2021.archive.ensembl.org")
+  genesV2 = getLDS(attributes = c("hgnc_symbol"), filters = "hgnc_symbol", values = x, mart = human, attributesL = c("mgi_symbol"), martL = mouse, uniqueRows=T)
   humanx <- unique(genesV2[, 2])
   # Print the first 6 genes found to the screen
   print(head(humanx))
   return(humanx)
+}
+
+## conversion of mouse gene symbols to human gene symbols
+convertMouseGeneList <- function(x){
+  require("biomaRt")
+  human = useMart(biomart="ensembl", dataset = "hsapiens_gene_ensembl", verbose = TRUE, host = "https://dec2021.archive.ensembl.org")
+  mouse = useMart(biomart="ensembl", dataset = "mmusculus_gene_ensembl", verbose = TRUE, host = "https://dec2021.archive.ensembl.org")
+  genesV2 = getLDS(attributes = c("mgi_symbol"), filters = "mgi_symbol", values = x, mart = mouse, attributesL = c("hgnc_symbol"), martL = human, uniqueRows=T)
+  mousex <- unique(genesV2[, 2])
+  # Print the first 6 genes found to the screen
+  print(head(mousex))
+  return(mousex)
 }
 
 ## Amino acid functions for TCR analyses
